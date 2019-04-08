788e1b51a3b9f8586e452b52dc91fb077a6eab99
largezhou <largezhou@hotmail.com>
2019-04-08 12:35:07 +0800

    帖子为空时，或者首次发帖，而是undefined导致的bug

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -140,14 +140,13 @@ export default {
       }
     },
     onNewPost(data) {
-      let t = this.$refs.postImages
-
       this.posts.unshift(data)
 
       // 由于加在前面的元素，在 refs 中，也是往后排的
       // 所以，这里手动把前面的新元素，放到 refs 中的前面
       // 用来与 posts 中的索引对应
       this.$nextTick(() => {
+        let t = this.$refs.postImages
         t = [
           t[t.length - 1],
           ...t.slice(0, t.length - 1),
@@ -186,9 +185,8 @@ export default {
     },
     initLazyLoad(oldPostLength) {
       this.$nextTick(() => {
-        this
-          .$refs
-          .postWraps
+        const postWraps = this.$refs.postWraps || []
+        postWraps
           .slice(oldPostLength)
           .forEach((el) => {
             this.lazyLoadObserver.observe(el)
