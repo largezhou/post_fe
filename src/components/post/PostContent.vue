fe94cf35c9c7e4695d6fed114b2fb7aebcc00478
largezhou <largezhou@hotmail.com>
2019-04-10 22:19:48 +0800

    简单的正则把url换成a标签

--- src/components/post/PostContent.vue
+++ src/components/post/PostContent.vue
@@ -10,7 +10,7 @@ export default {
   },
   computed: {
     htmlContent() {
-      return this.doubleSharpToLink(this.nToBr(this.content))
+      return this.linkToLink(this.doubleSharpToLink(this.nToBr(this.content)))
     },
   },
   mounted() {
@@ -28,6 +28,11 @@ export default {
     doubleSharpToLink(str) {
       return str.replace(/#(\s*[^\s#][^#]*)#/g, (match, p1) => {
         return `<a href="/tags/${p1.trim()}/posts" data-to="go-to-tag">${match}</a>`
+      })
+    },
+    linkToLink(str) {
+      return str.replace(/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ./?%&=]*)?/, (match) => {
+        return `<a href="${match}" target="_blank">${match}</a>`
       })
     },
   },
