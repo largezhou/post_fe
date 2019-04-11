f2c8d1433d454eceb93e6821c2bc90f1fc6c47bf
largezhou <largezhou@hotmail.com>
2019-04-11 22:14:17 +0800

    标签生成链接，用动态组件渲染成router-link，而非用点击事件触发router.push。

--- src/components/post/PostContent.vue
+++ src/components/post/PostContent.vue
@@ -1,8 +1,11 @@
 <template>
-  <span v-html="htmlContent"/>
+  <span ref="span"/>
 </template>
 
 <script>
+import Vue from 'vue'
+import router from '@/router'
+
 export default {
   name: 'PostContent',
   props: {
@@ -10,30 +13,54 @@ export default {
   },
   computed: {
     htmlContent() {
-      return this.linkToLink(this.doubleSharpToLink(this.nToBr(this.content)))
+      return this.linkToLink(this.nToBr(this.content))
     },
   },
   mounted() {
-    document.querySelectorAll('[data-to=go-to-tag]').forEach((a) => {
-      a.addEventListener('click', (e) => {
-        this.$router.push(e.target.pathname)
-        e.preventDefault()
-      })
-    })
+    this.doubleSharpToLink(this.htmlContent)
   },
   methods: {
     nToBr(str) {
       return str.replace(/\n/g, '<br>')
     },
-    doubleSharpToLink(str) {
-      return str.replace(/#(\s*[^\s#][^#]*)#/g, (match, p1) => {
-        return `<a href="/tags/${p1.trim()}/posts" data-to="go-to-tag">${match}</a>`
-      })
-    },
     linkToLink(str) {
       return str.replace(/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ./?%&=]*)?/, (match) => {
         return `<a href="${match}" target="_blank">${match}</a>`
       })
+    },
+    doubleSharpToLink(str) {
+      const span = this.$refs.span
+      const matches = str.matchAll(/#(\s*[^\s#][^#]*)#/g)
+      let lastStrIndex = 0
+
+      Array
+        .from(matches)
+        .forEach((m) => {
+          const match = m[0]
+          const p1 = m[1]
+          const index = m['index']
+
+          const text = str.slice(lastStrIndex, index)
+          if (text) {
+            span.appendChild(document.createTextNode(text))
+          }
+          const Comp = Vue.extend({
+            router,
+            render: (h) => h('router-link', {
+              props: {
+                to: `/tags/${p1.trim()}/posts`,
+              },
+            }, match),
+          })
+          const vm = new Comp()
+          span.appendChild(vm.$mount().$el)
+          lastStrIndex = index + match.length
+        })
+
+      // 补上最后部分文字
+      if (lastStrIndex !== str.length) {
+        span.appendChild(document.createTextNode(str.slice(lastStrIndex)))
+      }
     },
   },
 }
